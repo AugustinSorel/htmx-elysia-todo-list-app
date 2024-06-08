f9d3ff2d08f85400df705ac4c5ea292caba99798
AugustinSorel <sorelaugustin@gmail.com>
2024-06-08 20:24:39 +0100

    f

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -41,14 +41,3 @@ export const sessionTable = pgTable("session", {
     mode: "date",
   }).notNull(),
 });
-
-export const dummy = pgTable("dummy", {
-  id: varchar("id", { length: 255 }).primaryKey(),
-  userId: varchar("user_id", { length: 255 })
-    .notNull()
-    .references(() => userTable.id),
-  expiresAt: timestamp("expires_at", {
-    withTimezone: true,
-    mode: "date",
-  }).notNull(),
-});
