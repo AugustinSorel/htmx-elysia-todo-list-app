b8b35bee9f7f71c1a83d5eb72f26e6ef9dc57117
AugustinSorel <sorelaugustin@gmail.com>
2024-06-08 20:29:28 +0100

    f

---
+++ drizzle/0003_watery_edwin_jarvis.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS "cool_stuff" (
+	"id" varchar(255) PRIMARY KEY NOT NULL,
+	"expires_at" timestamp with time zone NOT NULL
+);
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -41,3 +41,11 @@ export const sessionTable = pgTable("session", {
     mode: "date",
   }).notNull(),
 });
+
+export const coolStuff = pgTable("cool_stuff", {
+  id: varchar("id", { length: 255 }).primaryKey(),
+  expiresAt: timestamp("expires_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull(),
+});
