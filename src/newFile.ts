460145aa9a61a7ce26a66243b634c0ee755fff78
AugustinSorel <sorelaugustin@gmail.com>
2024-06-11 19:15:03 +0100

    REFACTOR/unused file removed

--- src/newFile.ts
+++
@@ -1,8 +0,0 @@
-import { Elysia } from "elysia";
-import staticPlugin from "@elysiajs/static";
-import { router } from "./router";
-
-new Elysia()
-  .use(staticPlugin({ noCache: Bun.env.NODE_ENV === "dev" }))
-  .use(router)
-  .listen(3000);
